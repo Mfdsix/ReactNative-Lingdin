12b44d6f0247fc6fd6bd6cfdc37aa59f45a273f2
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-11-22 09:46:50 +0700

    add user and job data

---
+++ src/data/User.js
@@ -0,0 +1,35 @@
+const USER = {
+    name: "Mahfudz Ainur Rif'an",
+    image: "https://media-exp1.licdn.com/dms/image/C4D03AQHQbf96iuZq9g/profile-displayphoto-shrink_400_400/0/1633058089819?e=1643241600&v=beta&t=CS92o3Z8K6x2tnthcatbc5kIS4heQEj0V3kExJJVEyI",
+    headline: "Mobile Developer at PT FAN INTEGRASI TEKNOLOGI",
+    address: "Bekasi, West Java, Indonesia",
+    statistics: {
+        connections: 41,
+        followers: 30,
+        views: 19,
+        postViews: 96,
+        searchAppearance: 1,
+    },
+    activities: [
+        {
+            thumbnail: "https://media-exp1.licdn.com/dms/image/C5622AQGapUg7Tr35zg/feedshare-shrink_160/0/1636962131273?e=1640217600&v=beta&t=JbzOY-r_JTGLSXcNnhmPPGUJYnKN2clUIpfctLwuy6M",
+            headline: "#portfolio #mobileapp #uiux #mobileappdesign #designinspirations #travel #activetravel #trips #tripplanning UI/UX Mobile App Design Implementation",
+            reactions: 10,
+            comments: 0,
+        },
+        {
+            thumbnail: "https://media-exp1.licdn.com/dms/image/C5622AQGRoq3ORqh-hQ/feedshare-shrink_160/0/1636359568509?e=1640217600&v=beta&t=3x1qOlcejJjNyQ-0Lta1n3NgbMV120riDVEpatjaZVw",
+            headline: "#portfolio #mobileapp #uiux #mobileappdesign #designinspirations #watches UI/UX Mobile App Design Implementation",
+            reactions: 7,
+            comments: 0,
+        },
+        {
+            thumbnail: "https://media-exp1.licdn.com/dms/image/C5622AQEooWC5L1E7Kw/feedshare-shrink_160/0/1636358850037?e=1640217600&v=beta&t=hOwuirzVX2adAqHeeLKyf1NTieI93D5SxP4FgpC4Dzo",
+            headline: "#portfolio #mobileapp #uiux #mobileappdesign #designinspirations #scooter  #trips #trackingapp UI/UX Mobile App Design Implementation",
+            reactions: 5,
+            comments: 0,
+        },
+    ],
+};
+
+export default USER;
